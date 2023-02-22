4a6b873dbcbe3c260e9c7ff44668ca0fb7000984
Tybook92 <112991709+Tybook92@users.noreply.github.com>
2023-02-22 13:54:41 -0600

    Intital.finish

--- js/site.js
+++ js/site.js
@@ -1,21 +1,21 @@
-//get the values from the Page
+//get the values from the user. We need to get the fizz and the buzz value.
 //starts or controller function
-function getValues(){
+function getValues() {
     //getvalues from the page
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
     
-    //We need validate our input
-    //parse into integers
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
+    //parse for numbers
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
-    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-        //we call generateNumbers
-       let numbers = generateNumbers(startValue, endValue);
-        //we call displayNumbers
-        displayNumbers(numbers);
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        
+        //we call fizzbuzz
+       let fbArray = FizzBuzz(fizzValue, buzzValue);
+        //call displayData and write the values to the screen
+        displayData(fbArray);
     
     } else {
         alert("You must enter integers");
@@ -25,44 +25,62 @@ function getValues(){
 
 }
 
-//generate numbers from the startvalue to the endValue
-//logic function(s)
-function generateNumbers(sValue, eValue){
-    let numbers = [];
+//do fizz buzz
+function FizzBuzz(fizzValue, buzzValue) {
+    let returnArray = [];
 
-    //we want to get all numbers from start to end
-    for (let index = sValue; index <= eValue; index++){
-        
-        //this will execute in a loop until index = eValue
-        numbers.push(index);
+    //loop form 1 to 100
+    for (let i = 1; i <= 100; i++) {
+        if (i % fizzValue == 0 && i %buzzValue == 0) {
+            returnArray.push('FizzBuzz');
+        } else if (i % fizzValue == 0) {
+            returnArray.push('Fizz');
+        } else if (i % buzzValue == 0) {
+            returnArray.push('Buzz');
+        } else {
+            returnArray.push(i);
+        }
     }
-
-    return numbers;
+   return returnArray;
 }
 
-//display the numbers and mark even numbers bold
-//display or view functions
-function displayNumbers(numbers) {
 
-    let className = "even"
+//loop over the array and create a tablerow for each item.
+function displayData(fbArray) {
+    //get the table body element from the  page
+    let tableBody = document.getElementById("results");
 
-    let templateRows = "";
+    //get the template row
+    let templateRow = document.getElementById("fbTemplate");
 
-    for (let index = 0; index < numbers.length; index++) {
+    //clear table first
+    tableBody.innerHTML ="";
+
+    for (let index = 0; index < fbArray.length; index += 5) {
         
-        let number = numbers[index];
+        let tableRow = document.importNode(templateRow.content, true);
 
-        if(number % 2 == 0){
-           className = "even"
-        }
-        else{
-             className = "odd"
-        }
-        templateRows += `<tr><td class="${className}" >${number}</td></tr>`;
+        //grab use the to put into array
+        let rowCols = tableRow.querySelectorAll("td");
         
+        rowCols[0].classList.add(fbArray[index]);
+        rowCols[0].textContent = fbArray[index];
+
+        rowCols[1].classList.add(fbArray[index + 1]);
+        rowCols[1].textContent = fbArray[index+1];
+
+        rowCols[2].classList.add(fbArray[index + 2]);
+        rowCols[2].textContent = fbArray[index+2];
+
+        rowCols[3].classList.add(fbArray[index + 3]);
+        rowCols[3].textContent = fbArray[index+3];
+
+        rowCols[4].classList.add(fbArray[index + 4]);
+        rowCols[4].textContent = fbArray[index+4];
+
+        tableBody.appendChild(tableRow);
+
     }
 
-    document.getElementById("results").innerHTML = templateRows;
-
-
+    //add all the rows to the table
 }
